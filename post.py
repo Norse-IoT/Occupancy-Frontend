67617d1a41e4d060e06a4bc10d75cc480569e9ab
CG <congodsted@protonmail.com>
2023-02-16 22:41:33 -0500

    Update test-push with timestamp

--- post.py
+++ post.py
@@ -3,7 +3,8 @@ import requests
 url = 'http://localhost:3000'
 myobj = {
     'sensor': 'a', 
-    'count': 1
+    'count': 1,
+    'timestamp': 'YYYY-MM-DD-HH-SS'
 }
 
 x = requests.post(url, json = myobj)
